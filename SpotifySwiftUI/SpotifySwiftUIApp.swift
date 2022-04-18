c5f0f77ab1e3d48565c1e01f30692f8c701c5f2b
TemetCosta <berkesaritas4458@gmail.com>
2022-04-19 00:41:16 +0300

    Initial Commit

---
+++ SpotifySwiftUI/SpotifySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifySwiftUIApp.swift
+//  SpotifySwiftUI
+//
+//  Created by Berke Sarıtaş on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
